c9d909ff9e406a4e0de15844be2d0d6d1694ffd2
Jobin Francis Thomas <jobinft1995@gmail.com>
2020-07-31 16:52:15 +0530

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+
+def home(request):
+	return render(request, 'home.html', {})
+def about(request):
+	return render(request, 'about.html', {})
+def service(request):
+	return render(request, 'service.html', {})
+def blog(request):
+	return render(request, 'blog.html', {})
+def details(request):
+	return render(request, 'details.html', {})
+def contact(request):
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		#send email
+		send_mail(
+			message_name,
+			message,
+			message_email,
+			['jobinft1995@gmail.com'],
+			fail_silently=False,
+			)
+		return render(request, 'contact.html', {'message_name': message_name})
+
+	else:
+
+		return render(request, 'contact.html', {})
